7cee9fd1cf6913e7248f5307581426ff76ee8ace
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-09-19 00:03:55 +0530

    Completed basic design for menu page Added images for four menu items

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,17 +1,24 @@
-let menuitems=["Cheeseburger","Chicken Nuggets","French Fries","Hot Dog","Grilled Chicken Sandwich","Veggie Burger","Milkshake (Chocolate/Vanilla/Strawberry)",
-    "Onion Rings","Fried Chicken Burger","Mozzarella Sticks"];
+import burger from "./images/burger.jpg";
+import nuggets from "./images/nuggets.jpg";
+import fries from "./images/fries.jpg";
+import hotdog from "./images/hotdog.jpg";
+
+let menuitems=["Fried Chicken Burger","Chicken Nuggets","French Fries","Hot Dog","Grilled Chicken Sandwich","Veggie Burger","Chicken Doner Pitta","Milkshake",
+    "Onion Rings","Mozzarella Sticks"];
 let menudesc=[
-    "A juicy chicken patty topped with melted cheese, lettuce, tomato, and your choice of condiments, all sandwiched in a toasted bun.",
+    "A crispy, golden-fried chicken fillet topped with fresh lettuce, tomato, and mayo, all served on a soft toasted bun for the perfect crunch and flavor in every bite.",
     "Crispy, golden-brown bite-sized chicken pieces, perfect for dipping in your favorite sauce.",
     "Freshly cut and fried to golden perfection, our crispy fries are the perfect side to any meal.",
     "A grilled chicken hot dog served in a soft bun with your choice of toppings like ketchup, mustard, and relish.",
     "Tender grilled chicken breast topped with fresh lettuce, tomato, and mayo, served on a soft bun.",
     "A hearty plant-based patty with lettuce, tomato, and onions, served with a side of crisp fries.",
+    "Tender, seasoned rotisserie chicken in warm pitta bread with fresh salad and your choice of sauces.",
     "Creamy, rich milkshake blended with your choice of classic flavors: chocolate, vanilla, or strawberry.",
     "Thick-cut onions battered and fried to crispy perfection, served with a side of dipping sauce.",
-    "A crispy, golden-fried chicken fillet topped with fresh lettuce, tomato, and mayo, all served on a soft toasted bun for the perfect crunch and flavor in every bite.",
     "Golden-fried mozzarella cheese sticks, crunchy on the outside, gooey on the inside, served with marinara sauce for dipping."
 ];
+
+let menuimgs=[burger,nuggets,fries,hotdog];
 
 export default function(){
     const maindiv=document.createElement('div');
@@ -28,7 +35,10 @@ export default function(){
         itemname.textContent=menuitem;
         const itemdesc=document.createElement('p');
         itemdesc.textContent=menudesc[index];
+        const image=document.createElement('img')
+        image.src=menuimgs[index];
         itemdiv.appendChild(itemname);
+        itemdiv.appendChild(image);
         itemdiv.appendChild(itemdesc);
         items.appendChild(itemdiv);
     })
